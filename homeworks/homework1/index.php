7ec0e44840a2dbd01fb7daa5e45a03863426f924
lucarrg <60187251+lucarrg@users.noreply.github.com>
2025-03-21 19:27:19 +0300

    HW1

---
+++ homeworks/homework1/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HOMEWORK 1</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+    <header class="header">
+        <div class="header__wrapper wrapper">
+            <img src="logo.png" alt="Логотип" class="header__logo">
+            <h1 class="header__title">Домашняя работа: Hello, World!</h1>
+        </div>
+    </header>
+    <main class="main">
+        <div class="wrapper">
+            <?php echo 
+            "Hello, World!"; 
+            ?>
+        </div>
+    </main>
+    <footer class="footer">
+        <div class="wrapper">
+            <p class="footer__text">Задание для самостоятельной работы. </p>
+            <p class="footer__text">Создать веб-страницу с динамическим контентом. Загрузить код в удаленный репозиторий.</p>
+        </div>
+    </footer>
+</body>
+</html>
