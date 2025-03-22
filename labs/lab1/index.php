06803321628463a43387d5d4c72b290eb18c7dfa
lucarrg <60187251+lucarrg@users.noreply.github.com>
2025-03-22 15:06:10 +0300

    поменяла одно задание

--- labs/lab1/index.php
+++ labs/lab1/index.php
@@ -8,14 +8,6 @@
     echo "a = $a b = $b гипотенуза: $c <br><br>";
     
 
-    //11
-    echo 'Задание 11<br>';
-    $quiter = 'Тише';
-    $go = 'едешь';
-    $further = 'дальше';
-    echo "$quiter $go - $further будешь.<br><br>";
-
-
     //14
     echo 'Задание 14<br>';
     $quiter = 'Тише';
@@ -38,5 +30,13 @@
     $a=7;
     $b=8;
     $z = $a ** $b > $b ** $a ? "$a ** $b =" . $a ** $b : "a = $a b = $b";
-    echo $z; 
+    echo "$z<br><br>"; 
+
+    
+    //44
+    echo 'Задание 44<br>';
+    $year=2022;
+    $month=3;
+    $day=2;
+    echo sprintf("Дата: %d-%02d-%02d", $year, $month, $day);
 ?>
