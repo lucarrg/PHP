f0ac3a9e7670144a039c7eb3638f1254d1a9dea8
lucarrg <60187251+lucarrg@users.noreply.github.com>
2025-03-24 19:36:29 +0300

    готовый калькулятор

--- Calculator/calculator.php
+++ Calculator/calculator.php
@@ -1,21 +1,82 @@
 <?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if (isset($_POST['expression'])) {
-        $expression = $_POST['expression'];
-        if (preg_match('/^[0-9+\-*/()]+$/', $expression)) {
-            try {
-                $result = evaluateExpression($expression);
-                echo $result;
-            } catch (Exception $e) {
-                echo 'Ошибка: ' . $e->getMessage();
-            }
-        } else {
-            echo 'Ошибка: Неверное выражение';
-        }
+
+if (isset($_POST['expression'])) {
+    $calcRes = calculate($_POST['expression']);
+
+    if (is_numeric($calcRes)) {
+        echo $calcRes;
+    } else {
+        echo 'Ошибка вычисления выражения: ' . $calcRes;
     }
 }
 
-function evaluateExpression($expression) {
-    return eval("return $expression;");
+/** строка - число? */
+function isnum($inputStr) {
+    return is_numeric($inputStr);
 }
-?>
+
+function calculate($inputExpr) {
+    if (empty($inputExpr)) {
+        return 'Выражение не задано!';
+    }
+
+    if (!validScob($inputExpr)) {
+        return 'Неправильная расстановка скобок!';
+    }
+
+    $procExpr = str_replace(' ', '', $inputExpr);
+
+    // Обработка отрицательных чисел в начале выражения
+    if (substr($procExpr, 0, 1) == '-') {
+        $procExpr = '0' . $procExpr;
+    }
+
+    // Выражения в скобках (рекурсивно)
+    while (preg_match('/\(([^()]+)\)/', $procExpr, $matchGrps)) {
+        $procExpr = str_replace($matchGrps[0], calculate($matchGrps[1]), $procExpr);
+    }
+
+    // Умножение и деление
+    $procExpr = performOp($procExpr, '/(\-?\d+\.?\d*)([\/\*])(\-?\d+\.?\d*)/', function ($num1, $op, $num2) {
+        if ($op == '/' && $num2 == 0) {
+            return 'Ошибка: Деление на ноль!';
+        }
+        return $op == '*' ? $num1 * $num2 : $num1 / $num2;
+    });
+
+    // Сложение и вычитание
+    $procExpr = performOp($procExpr, '/(\-?\d+\.?\d*)([\+\-])(\-?\d+\.?\d*)/', function ($num1, $op, $num2) {
+        return $op == '+' ? $num1 + $num2 : $num1 - $num2;
+    });
+
+    return $procExpr;
+}
+
+/** Вспомогательная функция для выполнения операций (умножение/деление или сложение/вычитание) */
+function performOp($expr, $patt, $opCallback) {
+    while (preg_match($patt, $expr, $matchGrps)) {
+        $opRes = $opCallback($matchGrps[1], $matchGrps[2], $matchGrps[3]);
+        // Проверяем, если результат - это ошибка
+        if (is_string($opRes) && strpos($opRes, 'Ошибка:') !== false) {
+            return $opRes; // Возвращаем сообщение об ошибке
+        }
+        $expr = str_replace($matchGrps[0], $opRes, $expr);
+    }
+    return $expr;
+}
+
+/** корректность расстановки скобок в выражении */
+function validScob($exprToCheck) {
+    $scobCount = 0;
+    for ($i = 0; $i < strlen($exprToCheck); $i++) {
+        if ($exprToCheck[$i] == '(') {
+            $scobCount++;
+        } elseif ($exprToCheck[$i] == ')') {
+            $scobCount--;
+            if ($scobCount < 0) {
+                return false;
+            }
+        }
+    }
+    return $scobCount == 0;
+}
